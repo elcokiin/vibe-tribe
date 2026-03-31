dd17a3d1b71b0f0a45203a665392c94c67aa2a28
Diego Tenjo <tenjodiego961@gmail.com>
2026-03-31 17:06:28 -0500

    hot-fix: env variables

--- packages/env/src/web.js
+++ packages/env/src/web.js
@@ -3,7 +3,7 @@ import { z } from "zod";
 export const env = createEnv({
     clientPrefix: "PUBLIC_",
     client: {
-        PUBLIC_SERVER_URL: z.url(),
+        PUBLIC_SERVER_URL: z.string().url(),
     },
     runtimeEnv: import.meta.env,
     emptyStringAsUndefined: true,
